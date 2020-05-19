c2d124ae600e864850db49573e496cb40c7d3041
Alban Fellows <albanf@macs-software.co.uk>
2020-05-19 20:51:48 +0100

    Reproduction project for issue https://github.com/ChilliCream/hotchocolate/issues/1961

---
+++ QueryTest.cs
@@ -0,0 +1,31 @@
+﻿using HotChocolate;
+using HotChocolate.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQL_Bug
+{
+    public class QueryType 
+    {
+        [UseFiltering]
+        public IEnumerable<TestObject> TestGet()
+        {
+            yield return new TestObject { Id = 1, Name = "Test 1" };
+            yield return new TestObject { Id = 2, Name = "Test 2" };
+            yield return new TestObject { Id = 3, Name = "Test 3" };
+            yield return new TestObject { Id = 4, Name = "Fourth Test" };
+            yield return new TestObject { Id = 5, Name = "Fifth Test" };
+            yield return new TestObject { Id = 6, Name = "Sixth Test" };
+            yield return new TestObject { Id = 7, Name = "More Tests" };
+        }
+    }
+
+    public class TestObject
+    {
+        [GraphQLNonNullType] public int? Id { get; set; }
+        [GraphQLNonNullType] public string Name { get; set; }
+    }
+
+}
